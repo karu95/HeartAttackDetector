f5c34d2a86c61fbe9c0421699af513368a1c9909
mkarunanayake <mevan.15@cse.mrt.ac.lk>
2018-07-01 11:51:36 +0530

    Initial commit

---
+++ app/src/main/java/lk/ac/mrt/cse/heartattackdetector/controllers/Login.java
@@ -0,0 +1,17 @@
+package lk.ac.mrt.cse.heartattackdetector.controllers;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import lk.ac.mrt.cse.heartattackdetector.R;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+
+}
